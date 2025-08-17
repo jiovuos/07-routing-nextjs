0377b256ac08e603f895f0f31fe90075504f84a2
Volodymyr <voldemar.v.lysenko@gmail.com>
2025-08-17 15:30:00 +0300

    Init HW7 project from HW6 base

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
+
+export const metadata: Metadata = {
+  title: "NoteHub",
+  description: "Notes made simple",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <TanStackProvider>
+          <Header />
+          {children}
+          <div id="modal-root"></div>
+          <Footer />
+        </TanStackProvider>
+      </body>
+    </html>
+  );
+}
